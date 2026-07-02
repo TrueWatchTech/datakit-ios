4eac6ac4f18d36c86f69e9a476035965ae4584b5
hulilei <huuuu1016@gmail.com>
2026-07-02 19:20:04 +0800

    chore: add GuanceSessionReplay umbrella header

---
+++ Sources/SessionReplay/Public/GuanceSessionReplay.h
@@ -0,0 +1,14 @@
+#ifndef GuanceSessionReplay_h
+#define GuanceSessionReplay_h
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GuanceSessionReplay.
+FOUNDATION_EXPORT double GuanceSessionReplayVersionNumber;
+
+//! Project version string for GuanceSessionReplay.
+FOUNDATION_EXPORT const unsigned char GuanceSessionReplayVersionString[];
+
+#import "FTSessionReplay.h"
+
+#endif /* GuanceSessionReplay_h */
